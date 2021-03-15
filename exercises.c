a7daf681d4c6851fcbe7d103aa6ce5cfe6ba95ac
Replit user <replituser@example.com>
2021-03-15 20:53:29 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -11,8 +11,9 @@ Función que recibe 3 parámetros (a, b y c),
 y en c almacena el valor de la suma de a más b.
 */
 
-void suma(int a, int b, int * c) {
-   
+void suma(int a, int b, int * c) 
+{
+   *c = a + b ;
 }
 
 /*
