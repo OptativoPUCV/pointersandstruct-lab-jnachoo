c1c0ff9aa01977da7e8aba9d0729eb9ae9eddf72
Replit user <replituser@example.com>
2021-03-17 21:14:27 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -100,10 +100,11 @@ Programe la función void asignarValor(Vector * v, int i, int valor),
 la cual asigna el valor a la posición i del vector v.
 */
 void asignarValor(Vector * v, int i, int valor) {
-  for(;i>0;i--)
+  printf("  %d  ",i);
+  /*for(;i>0;i--)
   {
-    printf("  %d  ",i);
-  }
+    
+  }*/
 }
 
 /*
